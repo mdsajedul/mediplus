9cebb096b0d3225c873e8a4b8070b15bc3ffcff5
mdsajedul <sajedulislms@gmail.com>
2021-10-18 16:23:29 +0600

    banner and nav half done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Header from './Pages/Shared/Header/Header';
+import Home from './Pages/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+            <Header></Header>
+            <Switch>
+                <Route path="/">
+                    <Home></Home>
+                </Route>
+            </Switch>
+        </Router>
     </div>
   );
 }
